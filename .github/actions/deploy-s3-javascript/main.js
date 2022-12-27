28825e3044bef91caccf69abbc2c769e18ed6867
Gilbert Subay <gilbertttsubay@gmail.com>
2022-12-27 16:05:38 +0700

    menambah outputs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -19,10 +19,10 @@ function run() {
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
+   const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
 
+    core.setOutput('website-url', websiteUrl) // sama dengan echo "step-result=success >> $GITHUB_OUTPUT"
 
-
-    core.notice("Hello form my custom javascript actions")
 }
 
 run()
