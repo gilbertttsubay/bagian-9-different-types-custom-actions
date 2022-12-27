7be6d400fb9d9d4db94a9a8717547bb121f3e22b
Gilbert Subay <gilbertttsubay@gmail.com>
2022-12-27 12:52:56 +0700

    ganti jadi require

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,10 @@
-// const core = import('@actions/core')
-// const github = import('@actions/github')
-// const exec = import('@actions/exec')
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
 
-import core from "@actions/core"
-import github from "@actions/github"
-import exec from "@actions/exec"
+// import core from "@actions/core"
+// import github from "@actions/github"
+// import exec from "@actions/exec"
 
 
 function run() {
